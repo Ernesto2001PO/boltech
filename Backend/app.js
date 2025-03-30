16cf56c839602b858ba833818b78cce093d174c9
ErnestoNur <111402458+Ernesto2001PO@users.noreply.github.com>
2025-03-30 13:03:13 -0400

    Configuracion para conexion

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors'); 
 const {db } = require('./config/db-config');
 const app = express();
+
+
 app.use(cors());
 
 
--- Backend/controllers/personaController.js
+++ Backend/controllers/personaController.js
@@ -1,14 +1,50 @@
-const {Persona} = require('../models/Persona');
+const { Persona } = require('../models/Persona');
+const zod = require('zod');
+
 
 // Obtener todos los estudiantes
-exports.getEstudiantes  = async (req, res) => {
-    try {
-        const estudiante = await Persona.sequelize.query('SELECT * FROM personas WHERE rol = 1');
-        res.json(estudiante[0]);
-      
-    } catch (error) {
-        console.error('Error al obtener estudantes:', error);
-        res.status(400).json({ error: error.message });
-    }
+exports.getEstudiantes = async (req, res) => {
+  try {
+    const estudiante = await Persona.sequelize.query('SELECT * FROM personas WHERE rol = 1');
+    res.json(estudiante[0]);
+
+  } catch (error) {
+    console.error('Error al obtener estudantes:', error);
+    res.status(400).json({ error: error.message });
+  }
 }
+
+exports.crearEstudiante = async (req, res) => {
+  const { nombre, apellido, email, contrasena } = req.body;
+  // Validar 
+  zod.object({
+    nombre: zod.string().min(1),
+    apellido: zod.string().min(1),
+    email: zod.string().email(),
+    contrasena: zod.string().min(6)
+  }).parse(req.body);
+
+  // Verificar si el email ya existe
+  const emailExistente = await Persona.findOne({ where: { email } });
+  if (emailExistente) {
+    return res.status(400).json({ error: 'El email ya está en uso' });
+
+  }
+
+  try {
+    const nuevoEstudiante = await Persona.create({
+      nombre,
+      apellido,
+      email,
+      contrasena,
+      rol: 1
+    });
+
+    res.status(201).json(nuevoEstudiante);
+  } catch (error) {
+    console.error('Error al crear estudiante:', error);
+    res.status(400).json({ error: error.message });
+  }
+}
+
 
